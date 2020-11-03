ccca5499e4fac44d081edea8f92180b01f4baf09
qq928672 <qq928076@gmail.com>
2020-11-03 17:19:24 +0800

    updata

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -18,7 +18,7 @@
     color: white;
 }
 </style>
-<nav class="navbar navbar-expand-lg"style=background-color:#fbf7f0;>
+<nav class="navbar navbar-expand-lg"style=background-color:#f4f4f4;>
   <a class="navbar-brand" href="#"style=color:#495464>{{$username}}</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
